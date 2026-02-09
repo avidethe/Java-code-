ddbd644917d3597aa142ff93c4ed609063b3e92c
Avinash dethe <detheavinash07@gmail.com>
2026-02-09 10:05:23 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,21 @@
+//accessing tha same class 
+class Method
+{
+
+	static int a=189;
+	static double b= 10.5;
+	
+	public static void mash()
+	{
+		System.out.println("static mathod");
+	}
+
+	public static void main(String [] args)
+	{
+		System.out.println(a);
+		System.out.println(b);
+		mash();
+	}
+	
+
+}
