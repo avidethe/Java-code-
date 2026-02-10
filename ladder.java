83cf3eb8571c19b1336a451e51122787e8c97695
Avinash dethe <detheavinash07@gmail.com>
2026-02-10 10:47:29 +0530

    Add files via upload

---
+++ ladder.java
@@ -0,0 +1,20 @@
+class ladder
+	{
+		public static void main(String...args)
+		{
+		int a = 20; 
+		int	b = 20;
+		if(a>b)
+			{
+				System.out.println("a is grather");
+			}
+		else if(b>a)	
+			{
+				System.out.println("b is grather"); 
+			}
+		else 
+			{
+				System.out.println("both are equal"); 
+			}
+		}
+	}
