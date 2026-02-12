1c3079c1d8b34a04d2df81eee4ca3f309273d419
Avinash dethe <detheavinash07@gmail.com>
2026-02-12 19:49:18 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,14 @@
+class relational
+{
+    public static void main(String[]args)
+    {
+        System.out.println(100>25);
+        System.out.println(256>56);
+       boolean res = 56 > 100;
+        System.out.println(100<25);
+        System.out.println(100<25);
+        System.out.println(10!=25);
+        System.out.println(10>=25);
+
+    }
+}
