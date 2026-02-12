a55f38bbcad46c971d696685e4315c6504f426d1
Avinash dethe <detheavinash07@gmail.com>
2026-02-12 19:50:20 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,10 @@
+class Laptop
+ {
+	public static void main(String []args)
+	{
+		System.out.println(new Laptop());
+		System.out.println(new Laptop());
+		System.out.println(new Laptop());
+		System.out.println(new Laptop());
+	}
+ }
